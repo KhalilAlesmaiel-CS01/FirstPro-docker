bc7efe6be34a02f4fb5ca62eba4f3bfda267ead8
khalil alesmaiel <khalilalesmael@gmail.com>
2024-06-05 14:01:57 +0300

    UPD commit

--- src/index.js
+++ src/index.js
@@ -17,29 +17,29 @@ redisClient.on('connect', () => console.log('Connected to Redis ..'));
 
 redisClient.connect();
 //connect db
-const DB_USER = 'postgres';
-const DB_PASSWORD = 'example';
-const DB_PORT = 5432;
-const DB_HOST = 'postgres';
-const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-const pgClient = new Client ({
-  connectionString: URI,
-})
-pgClient.connect()
-  .then(() => console.log('Connected to postgres DB ..'))
-  .catch((err) => console.error(`Failed to connect to postgres DB:`,err));
-
-//const DB_USER = 'root';
+//const DB_USER = 'postgres';
 //const DB_PASSWORD = 'example';
-//const DB_PORT = 27017;
-//const DB_HOST = 'mongo';
-//const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-//mongoose
- // .connect(URI, {
-  // useNewUrlParser: true,
-   //useUnifiedTopology: true})
-    // .then(() => console.log('Connected to MongoDB ..'))
-     //.catch((err) => console.error(`Failed to connect to MongoDB:`,err));
+//const DB_PORT = 5432;
+//const DB_HOST = 'postgres';
+//const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+//const pgClient = new Client ({
+ // connectionString: URI,
+//})
+//pgClient.connect()
+//  .then(() => console.log('Connected to postgres DB ..'))
+//  .catch((err) => console.error(`Failed to connect to postgres DB:`,err));
+
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo';
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true})
+     .then(() => console.log('Connected to MongoDB ..'))
+     .catch((err) => console.error(`Failed to connect to MongoDB:`,err));
 
 app.get('/', (req, res) => {
   redisClient.set('products','products ...');
